8f2ac51267dc344b13128ed9e88527622a43efad
h.azizkhani <azizkhani.develop@gmail.com>
2022-08-12 23:12:31 +0200

    initial commit

---
+++ projects/ng-quicksearch/src/lib/ng-quicksearch.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NgQuicksearchComponent } from './ng-quicksearch.component';
+
+describe('NgQuicksearchComponent', () => {
+  let component: NgQuicksearchComponent;
+  let fixture: ComponentFixture<NgQuicksearchComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ NgQuicksearchComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(NgQuicksearchComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
